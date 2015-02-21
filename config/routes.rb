169c1c48d26f68d34e27aeabbd64e6f3bdef46c5
malnakli <hassoun@outlook.com>
2015-02-21 18:00:26 -0500

    add the routes for create a project

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'main_page#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+   ## ------ project ------ ##
+   post 'project/create' => 'project#create'
+   ## ---- end project ---- ##
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
