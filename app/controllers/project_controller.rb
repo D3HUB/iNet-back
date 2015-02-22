b10c2c15f4cafbdce1b0407e4c4b024cfd7403fd
malnakli <hassoun@outlook.com>
2015-02-22 09:53:19 -0500

    add project description page

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,8 +4,16 @@ class ProjectController < ApplicationController
         parameters = params.require(:projectCreate).permit(:name,:describe)
         result = ProjectBLL.instance.create(parameters[:name],parameters[:description])
 
-        unless result[:error].nil?
+        if result[:error].nil?
             render json: {message:'successful'}
+        end
+        render json: {message:result[:error]}
+    end
+
+    def get
+        result = ProjectBLL.instance.retrieve(params[:id])
+        if result[:error].nil?
+            render 'project/description' , locals: {name:result[:value][:name]} and return
         end
         render json: {message:result[:error]}
     end
